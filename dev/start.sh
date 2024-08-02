95b93e6dd10790e996fcbe6a0860a0000afe0aac
Taothong98 <TaothongSSS98@gmail.com>
2024-08-02 15:03:48 +0700

    :+1:

--- dev/start.sh
+++ dev/start.sh
@@ -6,20 +6,11 @@ echo ""
 /usr/share/logstash/bin/logstash -f /etc/logstash/conf.d/logstash.conf
 echo ""
 
-service supervisor start
-supervisord -c /etc/supervisor/supervisord.conf 
-echo ""
-
-
 cheak=echo "/home/link/cheakdir.sh"
 eval $check 
 echo ""
 
  
-# /usr/share/logstash/bin/logstash -f /etc/logstash/conf.d/logstash.conf
-
-
-# service --status-all -
 
 
 
